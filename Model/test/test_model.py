3d4b673de796f325025213097ca018d1d5b9a594
bulletcross <bulletcross@gmail.com>
2018-03-30 23:43:59 +0530

    added database code

--- Model/test/test_model.py
+++ Model/test/test_model.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from keras.models import Model, model_from_json
 from keras.optimizers import Adam
-
+import os
 import utilities
 
 def build_inference_graph(input_shape, hyper_param, output_shape):
@@ -25,13 +25,14 @@ def build_inference_graph(input_shape, hyper_param, output_shape):
         layer = Conv2D(filters = hp_filters[i], kernel_size = hp_kernel[i],
                     strides = hp_kernel_s[i], padding='same', activation='relu',
                     kernel_initializer='glorot_uniform')(layer)
-        layer = maxPooling2D(pool_size = hp_pool[i], strides = hp_pool_s[i](layer)
-    global_avgpool = Global_AveragePooling2D()(layer)
-    fc_layer = Dense(units = 64, activation='relu',
-                    kernel_initializer='glorot_uniform')(global_avgpool)
-    fc_layer = Dense(units = output_shape, activation='relu',
-                    kernel_initializer='glorot_uniform')(fc_layer)
-    model = Model(inputs = model_input, outputs = fc_layer)
+        layer = MaxPooling2D(pool_size = hp_pool[i], strides = hp_pool_s[i])(layer)
+
+    layer = GlobalAveragePooling2D()(layer)
+    layer = Dense(units = 64, activation='relu',
+                    kernel_initializer='glorot_uniform')(layer)
+    layer = Dense(units = output_shape, activation='relu',
+                    kernel_initializer='glorot_uniform')(layer)
+    model = Model(inputs = model_input, outputs = layer)
     return model
 
 def save_model(model, model_name):
@@ -40,6 +41,7 @@ def save_model(model, model_name):
         json_file.write(model_json)
     model.save_weights(model_name+".h5")
     print("Saved model in the name:" + model_name)
+    return
 
 def load_model(model_name):
     json_file = open(model_name+".json","r")
@@ -47,7 +49,7 @@ def load_model(model_name):
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     loaded_model.load_weights(model_name+".h5")
-return loaded_model
+    return loaded_model
 
 def main():
     (x_train, y_train) = utilities.load_dataset()
@@ -55,6 +57,8 @@ def main():
         print("Model found, loading...")
         model_def = load_model('trained_model')
     else:
+        input_shape = [384, 256, 3]
+        output_shape = 3
         model_def = build_inference_graph(input_shape, utilities.hyper_param, output_shape)
     model_def.summary()
     model_def.compile(optimizer = Adam(lr=0.001), loss = 'mse', metrics=['accuracy'])
