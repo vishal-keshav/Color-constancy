dd306db403035f869a949bff765771a3681a47db
bulletcross <bulletcross@gmail.com>
2018-04-14 17:26:03 +0530

    correction script for batches of images

--- Dataset_processing_scripts/image_correction_tool.py
+++ Dataset_processing_scripts/image_correction_tool.py
@@ -18,30 +18,55 @@ def apply_mask(img, msk):
     idx=(msk==1)
     img[idx] = 0
     return img
-
-def main():
-    img = cv2.imread('IMG5.png')
-    blackLevel = 10;
+"""
+Pass None as argumets if not known
+"""
+def create_mask(img, init_mask, blackLevel, saturationLevel):
+    # Images are expected to be parsed in BGR, default by cv2
     (B, G, R) = cv2.split(img)
-    saturationLevel = np.maximum(np.maximum(np.max(R), np.max(G)), np.max(B)) - 2
-    print(saturationLevel)
+    if saturationLevel == None:
+        saturationLevel =np.maximum(np.maximum(np.max(R), np.max(G)),
+                            np.max(B)) - 2
+    if blackLevel == None:
+        blackLevel = 10 # Assuming 8 bit depth per channel
     B = B - blackLevel
     G = G - blackLevel
     R = R - blackLevel
+    # Remove negatives
     R[R < 0] = 0
     G[G < 0] = 0
     B[B < 0] = 0
-    mask = np.zeros((img.shape[0], img.shape[1]))
-    mask = mask + (R>=saturationLevel-blackLevel).astype("uint8")
-    mask = mask + (G>=saturationLevel-blackLevel).astype("uint8")
-    mask = mask + (B>=saturationLevel-blackLevel).astype("uint8")
-    mask = mask > 0
-    mask[200:, 200:] = 1
+    if init_mask.all() == None:
+        init_mask = np.zeros((img.shape[0], img.shape[1]))
+    init_mask = init_mask + (R>=saturationLevel-blackLevel).astype("uint8")
+    init_mask = init_mask + (G>=saturationLevel-blackLevel).astype("uint8")
+    init_mask = init_mask + (B>=saturationLevel-blackLevel).astype("uint8")
+    init_mask = init_mask > 0
     img = cv2.merge([B, G, R])
-    show_image(img)
+    return img, init_mask
+
+def correct_image(img, mask_info):
+    if mask_info!=None:
+        #Logic to create mask from mask info here
+        mask = None
+        pass
+    else: # Remove else part
+        mask = np.zeros((img.shape[0], img.shape[1]))
+        mask[200:, 200:] = 1
+    img, mask = create_mask(img, mask, 10, None)
     img = apply_mask(img, mask)
-    show_image(img)
-    img = adjust_gamma(img, gamma = 2.2)
+    img = adjust_gamma(img, 2.2)
+    return img
+
+def correct_image_list(img_list, mask_info_list):
+    assert len(img_list) == len(mask_info_list)
+    for i in range(len(img_list)):
+        img_list[i] = correct_image(img_list[i], mask_info_list[i])
+    return img_list
+
+def main():
+    img = cv2.imread('IMG5.png')
+    img = correct_image(img, None)
     show_image(img)
 
 if __name__ == "__main__":
